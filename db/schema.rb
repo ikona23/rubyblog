3fa3a93a6918e9e7d133afc92e84221c16aff53e
ikona23@gmail.com <ikona23@gmail.com>
2015-01-30 00:57:38 +1300

    css

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141228102642) do
 
-  create_table "blogs", force: true do |t|
-    t.string   "title"
-    t.string   "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
